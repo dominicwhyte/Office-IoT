8e5a183a944b455ce597cf27f235a49f3693b14d
Dominic Whyte <dwhyte@princeton.edu>
2016-11-14 11:03:37 -0500

    Add files via upload

---
+++ Relayr Smart Office App/Pods/Target Support Files/Pods-Relayr Smart Office App/Pods-Relayr Smart Office App-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Relayr_Smart_Office_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Relayr_Smart_Office_AppVersionString[];
+
---
+++ Relayr Smart Office App/Relayr Smart Office App/AlternateTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlternateTableViewCell.swift
+//  Relayr Smart Office App
+//
+//  Created by Dominic Whyte on 22/07/16.
+//  Copyright © 2016 Relayr. All rights reserved.
+//
+
+import UIKit
+
+class AlternateTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
